101f192e7d03c7c74332e3c142adfec920eff7e1
Yarina Barabash <ybarabash@viju.ru>
2024-01-29 23:02:19 +0300

    first task

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <img alt="Vue logo" src="./assets/5495723.png" class="img-logo">
+  <FirstTask/>
+</template>
+
+<script>
+import FirstTask from './components/FirstTask.vue'
+
+export default {
+  name: 'App',
+  components: {
+   FirstTask
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.img-logo {
+  width: 150px;
+
+}
+</style>
---
+++ src/components/FirstTask.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <input name="myName" v-model="myName" placeholder="Введи имя">
+    <button @click="onClick" class="input-botton">
+      Нажми меня
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      myName: ""
+    };
+  },
+  methods: {
+    onClick() {
+      console.log(this.myName);
+    }
+  }
+}
+</script>
+
+<style>
+.input-botton {
+  background-color: aquamarine;
+  height: 20px;
+  border-radius: 4px;
+  border: none;
+  transition: all 300ms;
+  margin-left: 10px;
+}
+.input-botton:hover {
+ background-color: rgb(107, 227, 227);
+}
+</style>
